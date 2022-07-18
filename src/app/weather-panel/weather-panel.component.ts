1cd58cbebd50dd472da46e35332f431a52abf63e
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-07-18 13:23:43 +0700

    Add FooterComponent

---
+++ src/app/weather-panel/weather-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-panel',
+  templateUrl: './weather-panel.component.html',
+  styleUrls: ['./weather-panel.component.scss']
+})
+export class WeatherPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
