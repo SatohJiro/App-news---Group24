8df802aa073238bb1b13bf7d342cfd34ec369b69
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-07-25 16:21:13 +0700

    update weather panel and  box-cate-content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,7 @@ import { MdbTransferModule } from 'mdb-angular-transfer';
 import { MdbMentionModule } from 'mdb-angular-mention';
 import { MdbCookiesManagementService } from 'mdb-angular-cookies-management';
 import { MdbStorageManagementService } from 'mdb-angular-storage-management';
-import { WeatherPanelComponent } from './components/weather-panel/weather-panel.component';
+import { WeatherPanelComponent } from './components/home/weather-panel/weather-panel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
@@ -61,6 +61,7 @@ import { NewsRowComponent } from './components/news-row/news-row.component';
 import { BoxSubNewsComponent } from './components/box-sub-news/box-sub-news.component';
 import { BoxListNewsComponent } from './components/box-sub-news/box-list-news/box-list-news.component';
 import { BoxCateListComponent } from './components/box-sub-news/box-cate-list/box-cate-list.component';
+import { BoxCateContentComponent } from './components/home/box-cate-content/box-cate-content.component';
 
 
 
@@ -83,6 +84,7 @@ import { BoxCateListComponent } from './components/box-sub-news/box-cate-list/bo
         BoxSubNewsComponent,
         BoxListNewsComponent,
         BoxCateListComponent,
+        BoxCateContentComponent,
 
 
 
---
+++ src/app/components/home/box-cate-content/box-cate-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-cate-content',
+  templateUrl: './box-cate-content.component.html',
+  styleUrls: ['./box-cate-content.component.scss']
+})
+export class BoxCateContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
