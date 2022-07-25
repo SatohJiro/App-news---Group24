76d854d427487e0230e36bb28e4b30b8b79d8e36
nguyenhoaibao01 <76415346+nguyenhoaibao01@users.noreply.github.com>
2022-07-25 12:36:43 +0700

    Bao (#17)
    * tin tuc theo hang ngang
    
    * create box-sub-new

---
+++ src/app/components/box-sub-news/box-list-news/box-list-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-list-news',
+  templateUrl: './box-list-news.component.html',
+  styleUrls: ['./box-list-news.component.scss']
+})
+export class BoxListNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
