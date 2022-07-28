5bf9c4def3c65b876795e1a2c8cf8dab4508826d
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-07-28 14:21:33 +0700

    Nta (#31)
    * update new branch from main
    
    * fix css box-cate-content
    
    * update box-home-ngang
    
    * update box-locations
    
    * update box-news-detail

---
+++ src/app/components/box-news-detail/header-news-detail/header-news-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header-news-detail',
+  templateUrl: './header-news-detail.component.html',
+  styleUrls: ['./header-news-detail.component.scss']
+})
+export class HeaderNewsDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
