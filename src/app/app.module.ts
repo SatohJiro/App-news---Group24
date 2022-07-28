29b0325f7795d1f43a3de7c050005d519b1f4d82
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-07-28 14:18:58 +0700

    Bao dev (#30)
    * new
    
    * upload box-media
    
    * update boxmedia
    
    Co-authored-by: nguyenhoaibao01 <19130016@st.hcmuaf.edu.vn>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,6 +74,8 @@ import { BoxNewsComponent } from './components/home/box-news-content/box-news/bo
 import { BoxHomeNgangComponent } from './components/home/box-home-ngang/box-home-ngang.component';
 import { BoxNewsItemComponent } from './components/home/box-home-ngang/box-news-item/box-news-item.component';
 import { BoxLocationsComponent } from './components/home/box-locations/box-locations.component';
+import { BoxMediaComponent } from './components/home/box-media/box-media.component';
+import { ItemComponent } from './components/news-row/item/item.component';
 
 
 
@@ -102,6 +104,8 @@ import { BoxLocationsComponent } from './components/home/box-locations/box-locat
                   BoxHomeNgangComponent,
                   BoxNewsItemComponent,
                   BoxLocationsComponent,
+                  BoxMediaComponent,
+                  ItemComponent,
 
 
   ],
