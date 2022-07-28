0a1fbad817c61761a7c4227c7da56be26b744015
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-07-28 10:30:19 +0700

    Nta (#28)
    * update new branch from main
    
    * fix css box-cate-content
    
    * update box-home-ngang

---
+++ src/app/components/home/box-home-ngang/box-home-ngang.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box-home-ngang',
+  templateUrl: './box-home-ngang.component.html',
+  styleUrls: ['./box-home-ngang.component.scss']
+})
+export class BoxHomeNgangComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
