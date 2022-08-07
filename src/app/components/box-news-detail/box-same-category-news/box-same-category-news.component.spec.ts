59eb9f64a588b1005d5799943d631ac4643a8825
Trần Đoàn Kiến Thức <104188331+theweirdboiz@users.noreply.github.com>
2022-08-07 21:52:12 +0700

    Update home (#41)
    * Update box-news-detail
    
    * Add box-same-category-news
    
    * Update home
    
    * Update home
    
    Co-authored-by: Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>

---
+++ src/app/components/box-news-detail/box-same-category-news/box-same-category-news.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BoxSameCategoryNewsComponent } from './box-same-category-news.component';
+
+describe('BoxSameCategoryNewsComponent', () => {
+  let component: BoxSameCategoryNewsComponent;
+  let fixture: ComponentFixture<BoxSameCategoryNewsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BoxSameCategoryNewsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoxSameCategoryNewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
