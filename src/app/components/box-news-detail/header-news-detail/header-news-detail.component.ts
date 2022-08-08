22f68c0bcaac50b26ff1ccf050363a83a1b8a930
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-08-08 17:48:14 +0700

    Nta (#44)
    * crawl data for news detail
    
    * crawl data for header and related component

--- src/app/components/box-news-detail/header-news-detail/header-news-detail.component.ts
+++ src/app/components/box-news-detail/header-news-detail/header-news-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, Input, OnInit} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
+import {ServerService} from "../../../services/server.service";
 
 @Component({
   selector: 'app-header-news-detail',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-news-detail.component.scss']
 })
 export class HeaderNewsDetailComponent implements OnInit {
+  @Input() url:String = '';
+  items: any[] = [];
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document: Document, private serverService: ServerService) {
+  }
 
   ngOnInit(): void {
+    this.items = this.serverService.getDataHeaderDetail(this.url);
   }
 
 }
