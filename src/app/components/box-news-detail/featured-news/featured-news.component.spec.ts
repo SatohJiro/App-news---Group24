a415780cf3cc7f1c205122d6bd8335e1dbe497cf
nguyenhoaibao01 <76415346+nguyenhoaibao01@users.noreply.github.com>
2022-08-08 20:49:29 +0700

    Bao (#46)
    * tin noi bat
    
    * tin noi bat sua
    
    * sua loi commit tin noi baat

---
+++ src/app/components/box-news-detail/featured-news/featured-news.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeaturedNewsComponent } from './featured-news.component';
+
+describe('FeaturedNewsComponent', () => {
+  let component: FeaturedNewsComponent;
+  let fixture: ComponentFixture<FeaturedNewsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FeaturedNewsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FeaturedNewsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/box-news-detail/featured-news/featured-news.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-news',
+  templateUrl: './featured-news.component.html',
+  styleUrls: ['./featured-news.component.scss']
+})
+export class FeaturedNewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
