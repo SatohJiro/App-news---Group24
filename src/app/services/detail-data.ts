221da9124cbd7b723d262b13a7e9e7f36ddf2a87
nguyenhoaibao01 <19130016@st.hcmuaf.edu.vn>
2022-08-15 14:01:39 +0700

    function crawl featured-news data

--- src/app/services/detail-data.ts
+++ src/app/services/detail-data.ts
@@ -17,6 +17,8 @@ export class DetailData implements Idata {
   getData(): void {
     // @ts-ignore
     this.data?.push([{"relatedNews":this.getRelatedNews()}]);
+    // @ts-ignore
+    this.data?.push([{"featuredNews":this.getFeaturedNews()}]);
   }
 
   getRelatedNews(): string[] {
@@ -35,6 +37,34 @@ export class DetailData implements Idata {
         const $ = this.cheerio.load(this.responseText);
         // @ts-ignore
         $('.news-relation-bottom > ul > li').each((i, el) => {
+          relatedNews.push({
+            id: i,
+            link: $(el).find(" a ").attr("href"),
+            image: $(el).find("a > img").attr("src"),
+            title: $(el).find("a.title").attr("href"),
+          });
+        })
+      }
+    }
+    return relatedNews;
+    // }
+  }
+  getFeaturedNews(): string[] {
+    const ajax = new XMLHttpRequest();
+    let relatedNews: any[] = [];
+    // ajax.timeout = 3000;
+    const url = `${this.corsAnywhere}/${this.url}`;
+    const asyns = true;
+    const method = "GET";
+    ajax.open(method, url, asyns);
+    ajax.send();
+    // @ts-ignore
+    ajax.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        // @ts-ignore
+        const $ = this.cheerio.load(this.responseText);
+        // @ts-ignore
+        $('.boxtinnoibat > ul > li').each((i, el) => {
           relatedNews.push({
             id: i,
             link: $(el).find(" a ").attr("href"),
