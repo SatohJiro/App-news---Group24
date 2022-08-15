4ab4f9ad9d03b03cfe09e60193a6af13e4332998
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-08-15 12:42:25 +0700

    generate structure data (#57)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ParentData} from "./services/parent-data";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mdb5-angular-ui-kit-pro-advanced';
   contentLoaded:boolean=false;
+  data: [] | undefined;
+  // @ts-ignore
+  pdata:ParentData= new ParentData();
+  ngOnInit() {
+    // @ts-ignore
+    this.data=  this.pdata.getDataHome();
+  }
+
 }
--- src/app/services/parent-data.ts
+++ src/app/services/parent-data.ts
@@ -1,5 +1,35 @@
 import {Idata} from "./idata";
+import {DetailData} from "./detail-data";
+import {HomeData} from "./home-data";
 
-export interface ParentData {
-data: Idata[];
+export class ParentData {
+  data: Idata[] | undefined;
+  pageData: Idata | undefined;
+  constructor() {
+    // @ts-ignore
+    this.getDataHome();
+  }
+
+// @ts-ignore
+  getDataHome(url: string): [] | undefined {
+    // @ts-ignore
+   this.pageData = new HomeData();
+    // @ts-ignore
+    this.data?.push(this.pageData?.getData());
+  };
+
+
+  addPage(url: string) {
+    // @ts-ignore
+    for (let i = 0; i < this.data?.length; i++) {
+      // @ts-ignore
+      if (this.data[i].url === url) {
+        return;
+      }
+    }
+    // @ts-ignore
+    this.pageData= new DetailData(url);
+    // @ts-ignore
+    this.data?.push(this.pageData?.getData());
+  }
 }
