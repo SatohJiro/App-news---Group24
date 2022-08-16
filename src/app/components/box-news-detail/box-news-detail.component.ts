f2e58a8d73585e768500db51875bb478156060f7
nguyenhoaibao01 <19130016@st.hcmuaf.edu.vn>
2022-08-16 17:20:33 +0700

    get multymedia

--- src/app/components/box-news-detail/box-news-detail.component.ts
+++ src/app/components/box-news-detail/box-news-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HomeService} from "../home/home.service";
 
 @Component({
   selector: 'app-box-news-detail',
@@ -7,12 +8,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BoxNewsDetailComponent implements OnInit {
   url: String = 'https://nld.com.vn/chinh-tri/nguyen-chu-tich-nuoc-truong-tan-sang-nhan-huy-hieu-50-nam-tuoi-dang-20220808143125822.htm'
-  constructor() {
+  constructor(private homeService: HomeService) {
+
   }
-
-
+  data:any = {};
+  newsMain:any = {}
+  listNews:any[] = []
 
   ngOnInit(): void {
+     this.newsMain = this.data.newsMain;
+     this.listNews = this.data.listNews;
   }
 
 }
--- src/app/components/home/home.service.ts
+++ src/app/components/home/home.service.ts
@@ -189,4 +189,42 @@ getListSubNews():INews[] {
   }
   return data;
 }
+
+  getMultimedia():any {
+    const ajax = new XMLHttpRequest();
+    let data: any = {};
+    // ajax.timeout = 3000;
+    const url = `${this.corsAnywhere}/${this.server}`;
+    const asyns = true;
+    const method = "GET";
+    ajax.open(method, url, asyns);
+    ajax.send();
+    // @ts-ignore
+    ajax.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const $ = cheerio.load(this.responseText);
+      let mainNews =  $('.box-media .main-news');
+        data.mainNews = {
+            id: "main",
+            title: $(mainNews).find('a').attr('title'),
+            link: $(mainNews).find('a').attr('href'),
+            urlImg: $(mainNews).find('a > img').attr('src')? $(mainNews).find('a > img').attr('src'): $(mainNews).find('a > video').attr('poster')
+          }
+
+        data.listSubNews = []
+        $('.box-media .news-item').each((i, item) => {
+          // @ts-ignore
+          data.listSubNews.push(
+            {
+              id: i,
+              title: $(item).find('a').attr('title'),
+              link: $(item).find('a').attr('href'),
+              urlImg: $(item).find('a > img').attr('src')? $(item).find('a > img').attr('src'): $(item).find('a > video').attr('poster')
+            }
+          )
+        })
+      }
+    }
+    return data;
+  }
 }
