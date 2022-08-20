af68c69bb3a1557deeb2418550312b9194baaf35
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-08-20 23:12:21 +0700

    update like and share button

--- src/app/components/box-news-detail/box-news-detail.component.ts
+++ src/app/components/box-news-detail/box-news-detail.component.ts
@@ -1,8 +1,6 @@
-
-
 import {Component, OnInit} from '@angular/core';
 
-import {ActivatedRoute,Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 
 import {ParentData} from "../../services/parent-data";
 import {DetailData} from "../../services/detail-data";
@@ -12,32 +10,31 @@ import {DetailData} from "../../services/detail-data";
   templateUrl: './box-news-detail.component.html',
   styleUrls: ['./box-news-detail.component.scss']
 })
-export class BoxNewsDetailComponent implements OnInit  {
+export class BoxNewsDetailComponent implements OnInit {
 
   dataPage: DetailData | null = null;
 
-  linkPage: string | null = null;
+  linkPage: string = "";
   headerLoading: boolean = true;
   detailLoading: boolean = true;
 
-  mostViewLoading: boolean =true;
+  mostViewLoading: boolean = true;
   dataHeader: [] = [];
   dataDetail: [] = [];
   dataRelated: [] = [];
-  dataMostView:any[]=[];
+  dataMostView: any[] = [];
 
 
-  constructor(private activatenRoute: ActivatedRoute,private router:Router , private dataParent: ParentData) {
+  constructor(private activatenRoute: ActivatedRoute, private router: Router, private dataParent: ParentData) {
 
   }
 
 
-
   getLinkPage() {
     this.activatenRoute.paramMap.subscribe(paramMap => {
-      this.linkPage = paramMap.get('link');
+      this.linkPage = String(paramMap.get('link'));
     })
-console.log(this.linkPage);
+    console.log(this.linkPage);
   }
 
   getDataPage(): void {
@@ -53,7 +50,7 @@ console.log(this.linkPage);
   ngOnInit(): void {
     this.headerLoading = true;
     this.detailLoading = true;
-    this.mostViewLoading =true;
+    this.mostViewLoading = true;
     this.getLinkPage();
 
     // @ts-ignore
@@ -61,7 +58,7 @@ console.log(this.linkPage);
     if (checkURL) {
       this.headerLoading = false;
       this.detailLoading = false;
-      this.mostViewLoading =false;
+      this.mostViewLoading = false;
     }
     if (this.mostViewLoading) {
       setTimeout(() => {
@@ -70,14 +67,14 @@ console.log(this.linkPage);
     }
     this.getDataPage();
 
-      // @ts-ignore
-      this.dataHeader = this.dataPage.data[0][0].headerData;
-      // @ts-ignore
-      this.dataDetail = this.dataPage.data[1][0].detailData;
-      // @ts-ignore
-      this.dataRelated=this.dataPage.data[2][0].relatedData;
     // @ts-ignore
-    this.dataMostView=this.dataPage.data[3][0].mostViewData;
+    this.dataHeader = this.dataPage.data[0][0].headerData;
+    // @ts-ignore
+    this.dataDetail = this.dataPage.data[1][0].detailData;
+    // @ts-ignore
+    this.dataRelated = this.dataPage.data[2][0].relatedData;
+    // @ts-ignore
+    this.dataMostView = this.dataPage.data[3][0].mostViewData;
     // @ts-ignore
 
   }
--- src/app/components/news/news-detail/news-detail.component.ts
+++ src/app/components/news/news-detail/news-detail.component.ts
@@ -9,13 +9,23 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class NewsDetailComponent implements OnInit {
+  @Input() url:string="";
   @Input() data: any[] = [];
   @Input() detailLoading: boolean = true;
+  liked:boolean=false;
+  likeText:string="";
 
 
   constructor() {
   }
-
+like(){
+    this.liked=!this.liked;
+    if (this.liked){
+      this.likeText="Đã Thích"
+    }else{
+      this.likeText="";
+    }
+}
   ngOnInit(): void {
     if (this.detailLoading) {
       setTimeout(() => {
