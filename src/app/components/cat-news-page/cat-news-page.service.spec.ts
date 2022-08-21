9f3c686d3ddced0d24c22c7a4ad02b8860f74536
nguyenhoaibao01 <19130016@st.hcmuaf.edu.vn>
2022-08-21 22:41:36 +0700

    get data category page from rss

---
+++ src/app/components/cat-news-page/cat-news-page.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CatNewsPageService } from './cat-news-page.service';
+
+describe('CatNewsPageService', () => {
+  let service: CatNewsPageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CatNewsPageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
