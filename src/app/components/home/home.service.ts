09a87f4a4b9d9694f909cd5b705db2eef76e5c95
Nguyễn Trần Anh <69859398+SatohJiro@users.noreply.github.com>
2022-08-23 01:47:37 +0700

    update router link home

--- src/app/components/home/home.service.ts
+++ src/app/components/home/home.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import * as cheerio from "cheerio";
-import {delay, Observable, of, shareReplay, map, switchMap, BehaviorSubject} from "rxjs";
+import {BehaviorSubject, delay, Observable, of, shareReplay, switchMap} from "rxjs";
 import {INews} from "../news/news";
 import {HttpClient} from "@angular/common/http";
 
@@ -24,124 +24,138 @@ export class HomeService {
   private _gocNhinNews$ = new BehaviorSubject<void>(undefined);
   private _listMostViewedNews$ = new BehaviorSubject<void>(undefined);
   //@ts-ignore
-  public apiListHotNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiListHotNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadListHotNews(data)),
   );
-  public listHotNews$ = this._listHotNews$.pipe(switchMap(()=> this.apiListHotNewsRequest$),shareReplay(1));
+  public listHotNews$ = this._listHotNews$.pipe(switchMap(() => this.apiListHotNewsRequest$), shareReplay(1));
   //@ts-ignore
 
-  public apiListNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}/${this.option}`,{responseType: 'text'}).pipe(
+  public apiListNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}/${this.option}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadListNews(data)),
   );
-  public listNews$ = this._listNews$.pipe(switchMap(()=> this.apiListNewsRequest$),shareReplay(1));
+  public listNews$ = this._listNews$.pipe(switchMap(() => this.apiListNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiTagsHotNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiTagsHotNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadTagsHotNew(data)),
   );
-  public tagsHotNews$ = this._tagsHotNews$.pipe(switchMap(()=> this.apiTagsHotNewsRequest$),shareReplay(1));
+  public tagsHotNews$ = this._tagsHotNews$.pipe(switchMap(() => this.apiTagsHotNewsRequest$), shareReplay(1));
 
 //@ts-ignore
-  public apiRowListNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiRowListNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadRowListNews(data)),
   );
-  public rowListNews$ = this._rowListNews$.pipe(switchMap(()=> this.apiRowListNewsRequest$),shareReplay(1));
+  public rowListNews$ = this._rowListNews$.pipe(switchMap(() => this.apiRowListNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiBoxListNews$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiBoxListNews$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadBoxListNews(data)),
   );
-  public boxListNews$ = this._boxListNews$.pipe(switchMap(()=> this.apiBoxListNews$),shareReplay(1));
+  public boxListNews$ = this._boxListNews$.pipe(switchMap(() => this.apiBoxListNews$), shareReplay(1));
 
   //@ts-ignore
-  public apiNoiThangNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiNoiThangNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadNoiThangNews(data)),
   );
-  public noiThangNews$ = this._noiThangNews$.pipe(switchMap(()=> this.apiNoiThangNewsRequest$),shareReplay(1));
+  public noiThangNews$ = this._noiThangNews$.pipe(switchMap(() => this.apiNoiThangNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiDocQuyenNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiDocQuyenNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadDocQuyenNews(data)),
   );
-  public docQuyenNews$ = this._docQuyenNews$.pipe(switchMap(()=> this.apiDocQuyenNewsRequest$),shareReplay(1));
+  public docQuyenNews$ = this._docQuyenNews$.pipe(switchMap(() => this.apiDocQuyenNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiHoiNongDapNhanhNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiHoiNongDapNhanhNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadHoiNongDapNhanhNews(data)),
   );
-  public hoiNongDapNhanhNews$ = this._hoiNongDapNhanhNews$.pipe(switchMap(()=> this.apiHoiNongDapNhanhNewsRequest$),shareReplay(1));
+  public hoiNongDapNhanhNews$ = this._hoiNongDapNhanhNews$.pipe(switchMap(() => this.apiHoiNongDapNhanhNewsRequest$), shareReplay(1));
 //@ts-ignore
-  public apiGocNhinNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiGocNhinNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadGocNhinNews(data)),
   );
-  public gocNhinNews$ = this._gocNhinNews$.pipe(switchMap(()=> this.apiGocNhinNewsRequest$),shareReplay(1));
+  public gocNhinNews$ = this._gocNhinNews$.pipe(switchMap(() => this.apiGocNhinNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiTruyVetMangXaHoiNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiTruyVetMangXaHoiNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadTruyVetMangXaHoiNews(data)),
   );
-  public truyVetMangXaHoiNews$ = this._truyVetMangXaHoiNews$.pipe(switchMap(()=> this.apiTruyVetMangXaHoiNewsRequest$),shareReplay(1));
+  public truyVetMangXaHoiNews$ = this._truyVetMangXaHoiNews$.pipe(switchMap(() => this.apiTruyVetMangXaHoiNewsRequest$), shareReplay(1));
 
   //@ts-ignore
-  public apiListMostViewedNewsRequest$ =  this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`,{responseType: 'text'}).pipe(
+  public apiListMostViewedNewsRequest$ = this.http.get<any[]>(`${this.corsAnywhere}/${this.server}`, {responseType: 'text'}).pipe(
     //@ts-ignore
     switchMap(data => this.loadListMostViewedNews(data)),
   );
-  public listMostViewedNews$ = this._listMostViewedNews$.pipe(switchMap(()=> this.apiListMostViewedNewsRequest$),shareReplay(1));
+  public listMostViewedNews$ = this._listMostViewedNews$.pipe(switchMap(() => this.apiListMostViewedNewsRequest$), shareReplay(1));
 
   constructor(private http: HttpClient) {
 
   }
-  loadTagsHotNew(text:string) {
+
+  loadTagsHotNew(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.hot-news-item > a').each((i, tag) => {
       // @ts-ignore
       data.push({
         id: $(tag).attr(''),
         title: $(tag).attr("title"),
-        link: $(tag).attr("href"),
+        // @ts-ignore
+        link:change($(tag).attr("href")) ,
         time: new Date().toISOString()
       });
     })
     return of(data).pipe(delay(50))
   }
-  loadListHotNews(text:string) {
-  const $ = cheerio.load(text);
-  let data:any[] = [];
-  $('.list-news-thumb .news-thumb-title > a').each((i, news) => {
-    // @ts-ignore
-    data.push({
-      id: $(news).attr('data-newsid'),
-      title: $(news).attr("title"),
-      link: $(news).attr("href"),
-      time: new Date().toISOString()
-    });
-  })
-  return of(data).pipe(delay(50))
-}
-  loadListNews(text:string) {
+
+  loadListHotNews(text: string) {
+    const $ = cheerio.load(text);
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
+    $('.list-news-thumb .news-thumb-title > a').each((i, news) => {
+      // @ts-ignore
+      data.push({
+        id: $(news).attr('data-newsid'),
+        title: $(news).attr("title"),
+        // @ts-ignore
+        link:change($(news).attr("href")) ,
+        time: new Date().toISOString()
+      });
+    })
+    return of(data).pipe(delay(50))
+  }
+
+  loadListNews(text: string) {
     const getImg = (text: string) => {
       const $ = cheerio.load(text, {xmlMode: true});
       return $('img');
     }
-    const $ = cheerio.load(text,{xmlMode:true});
-    let data:any[] = [];
+    const $ = cheerio.load(text, {xmlMode: true});
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('item').each((i, item) => {
       data.push({
         id: i,
         title: $(item).find('title').text().trim(),
-        link: $(item).find('link').text().trim(),
+        link: change($(item).find('link').text().trim()),
         guid: $(item).find('guid').text().trim(),
         description: {
           // @ts-ignore
@@ -155,15 +169,20 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
-  loadRowListNews(text:string) {
+
+  loadRowListNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.news-horizontal-item > a').each((i, news) => {
       // @ts-ignore
       data.push({
         id: $(news).attr('data-newsid'),
         title: $(news).attr("title"),
-        link: $(news).attr("href"),
+        // @ts-ignore
+        link: change($(news).attr("href")),
         description: {
           imgUrl: $(news).find('img').attr('src'),
           alt: $(news).find('img').attr('alt'),
@@ -172,38 +191,50 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
-  loadBoxListNews(text:string) {
+
+  loadBoxListNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-news-list:first-of-type .news-item').each((i, news) => {
       // @ts-ignore
       data.push({
         id: $(news).attr('data-newsid'),
         title: $(news).find('.img220x139').attr("title"),
-        link: $(news).find('.img220x139').attr("href"),
+        // @ts-ignore
+        link: change($(news).find('.img220x139').attr("href")),
         description: {
           imgUrl: $(news).find('.img220x139 > img').attr('src') ? $(news).find('.img220x139 > img').attr('src') : $(news).find('.img220x139 > video').attr('poster'),
           alt: $(news).find('.img220x139 > img').attr('alt'),
         },
         infor: {
           title1: $(news).find('.news-info ul li:first-of-type > a').attr('title'),
-          link1: $(news).find('.news-info ul li:first-of-type > a').attr('href'),
+          // @ts-ignore
+          link1: change($(news).find('.news-info ul li:first-of-type > a').attr('href')),
           title2: $(news).find('.news-info ul li:last-of-type > a').attr('title'),
-          link2: $(news).find('.news-info ul li:last-of-type > a').attr('href'),
+          // @ts-ignore
+          link2: change($(news).find('.news-info ul li:last-of-type > a').attr('href')),
         }
       });
     })
     return of(data).pipe(delay(50))
   }
-  loadNoiThangNews(text:string) {
+
+  loadNoiThangNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-cate-list .box-noithang .news-item > .img216x133').each((i, news) => {
       // @ts-ignore
       data.push({
         id: i + '',
         title: $(news).attr("title"),
-        link: $(news).attr("href"),
+        // @ts-ignore
+        link: change($(news).attr("href")),
         description: {
           imgUrl: $(news).find('img').attr('src'),
           alt: $(news).find('img').attr('alt'),
@@ -212,6 +243,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getListHotNews(): Observable<INews[]> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -336,15 +368,19 @@ export class HomeService {
   //   }
   //   return of(data).pipe(delay(50));
   // }
-  loadDocQuyenNews(text:string) {
+  loadDocQuyenNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-cate-list .box-docquyen .news-item > .img216x133').each((i, news) => {
       // @ts-ignore
       data.push({
         id: i + '',
         title: $(news).attr("title"),
-        link: $(news).attr("href"),
+        // @ts-ignore
+        link: change($(news).attr("href")),
         description: {
           imgUrl: $(news).find('img').attr('src'),
           alt: $(news).find('img').attr('alt'),
@@ -353,6 +389,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getNewsDocQuyen(): Observable<INews[]> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -408,14 +445,18 @@ export class HomeService {
   //   }
   //   return of(data).pipe(delay(50));
   // }
-  loadHoiNongDapNhanhNews(text:string) {
+  loadHoiNongDapNhanhNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-hoi-nong-dap-nhanh').each((i, news) => {
       // @ts-ignore
       data.push({
         id: i + '',
-        title: $(news).find('.box-hom-ngang-sub-content-thumb').attr("title"),
+        // @ts-ignore
+        title: change($(news).find('.box-hom-ngang-sub-content-thumb').attr("title")),
         link: $(news).find('.box-hom-ngang-sub-content-thumb').attr("href"),
         description: {
           imgUrl: $(news).find('img').attr('src'),
@@ -425,6 +466,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getHoiNongDapNhanh(): Observable<INews> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -452,15 +494,19 @@ export class HomeService {
   //   }
   //   return of(data).pipe(delay(50));
   // }
-  loadTruyVetMangXaHoiNews(text:string) {
+  loadTruyVetMangXaHoiNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-truy-vet-mang-xa-hoi').each((i, news) => {
       // @ts-ignore
       data.push({
         id: i + '',
         title: $(news).find('.box-hom-ngang-sub-content-thumb').attr("title"),
-        link: $(news).find('.box-hom-ngang-sub-content-thumb').attr("href"),
+        // @ts-ignore
+        link: change($(news).find('.box-hom-ngang-sub-content-thumb').attr("href")),
         description: {
           imgUrl: $(news).find('.box-hom-ngang-sub-content-thumb > img').attr('src'),
           alt: $(news).find('.box-hom-ngang-sub-content-thumb > img').attr('alt'),
@@ -469,6 +515,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getTruyVetMangXaHoi(): Observable<INews> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -496,15 +543,19 @@ export class HomeService {
   //   }
   //   return of(data).pipe(delay(50));
   // }
-  loadGocNhinNews(text:string) {
+  loadGocNhinNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-goc-nhin').each((i, news) => {
       // @ts-ignore
       data.push({
         id: i + '',
         title: $(news).find('.box-hom-ngang-sub-content-thumb').attr("title"),
-        link: $(news).find('.box-hom-ngang-sub-content-thumb').attr("href"),
+        // @ts-ignore
+        link: change($(news).find('.box-hom-ngang-sub-content-thumb').attr("href")),
         description: {
           imgUrl: $(news).find('.box-hom-ngang-sub-content-thumb > img').attr('src'),
           alt: $(news).find('.box-hom-ngang-sub-content-thumb > img').attr('alt'),
@@ -513,6 +564,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getGocNhin(): Observable<INews> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -540,15 +592,19 @@ export class HomeService {
   //   }
   //   return of(data).pipe(delay(50));
   // }
-  loadListMostViewedNews(text:string) {
+  loadListMostViewedNews(text: string) {
     const $ = cheerio.load(text);
-    let data:any[] = [];
+    let data: any[] = [];
+    const change = (temp: string) => {
+      return this.changeUrlPattern(temp);
+    }
     $('.box-news-container .news-item').each((i, news) => {
       // @ts-ignore
       data.push({
         id: $(news).attr('data-newsid'),
         title: $(news).find('.img220x139').attr("title"),
-        link: $(news).find('.img220x139').attr("href"),
+        // @ts-ignore
+        link: change($(news).find('.img220x139').attr("href")),
         description: {
           imgUrl: $(news).find('.img220x139 > img').attr('src') ? $(news).find('.img220x139 > img').attr('src') : $(news).find('.img220x139 > video').attr('poster'),
           alt: $(news).find('.img220x139 > img').attr('alt'),
@@ -563,6 +619,7 @@ export class HomeService {
     })
     return of(data).pipe(delay(50))
   }
+
   // getBoxMostViewed(): Observable<INews> {
   //   const url = `${this.corsAnywhere}/${this.server}`;
   //   const ajax = new XMLHttpRequest();
@@ -878,4 +935,12 @@ export class HomeService {
   // testRxJS() {
   //
   // }
+  changeUrlPattern(url: string): string {
+    let pattern: string = "https://nld.com.vn";
+    if (url.startsWith(pattern)) {
+      return url.replace(pattern, "");
+    } else {
+      return url;
+    }
+  }
 }
